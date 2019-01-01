3a8191af3555933be8d0ce6dab03653e2bade391
Sakari De-Meis <sakaridemeis@gmail.com>
2019-01-01 20:56:54 +0100

    change colour of icon and increase text margin on card to reduce image border to match interests/travel and friends/family

--- app/src/main/java/app/bluecoral/MinimaLista/ListModel/MainActivity.kt
+++ app/src/main/java/app/bluecoral/MinimaLista/ListModel/MainActivity.kt
@@ -3,8 +3,8 @@ package app.bluecoral.MinimaLista.ListModel
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView
 import app.bluecoral.MinimaLista.R
+import app.bluecoral.MinimaLista.R.id.toolbar
 import app.bluecoral.MinimaLista.TodoModel.*
 import kotlinx.android.synthetic.main.activity_main_list.*
 
